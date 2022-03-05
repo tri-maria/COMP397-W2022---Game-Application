96165045de60e157ca443b0ed7e51720682bedcb
Maria Albufera <malbufer@my.centennialcollege.ca>
2022-03-05 18:29:31 -0500

    Add my Week 7 Cloned Tillable Assets

--- WEEK7-COMP397/Assets/[Scripts]/MapGenerator.cs
+++ WEEK7-COMP397/Assets/[Scripts]/MapGenerator.cs
@@ -4,93 +4,61 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    [Header("Tile Resources")] 
+    [Header("Tile Resources")]
     public List<GameObject> tilePrefabs;
-    public GameObject startTile;
-    public GameObject goalTile;
 
-    [Header("Map Properties")] 
+    [Header("Map Properties")]
     [Range(2, 30)]
     public int width = 2;
-    [Range(2, 30)] 
+    [Range(2, 30)]
     public int depth = 2;
-    public Transform parent;
 
-    [Header("Generated Tiles")] 
+    [Header("Generated Tiles")]
     public List<GameObject> tiles;
 
-    private int startWidth;
-    private int startDepth;
 
     // Start is called before the first frame update
     void Start()
     {
-        startWidth = width;
-        startDepth = depth;
         BuildMap();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (width != startWidth || depth != startDepth)
-        {
-            ResetMap();
-            BuildMap();
-        }
+
     }
-
-    public void ResetMap()
-    {
-        startWidth = width;
-        startDepth = depth;
-        var size = tiles.Count;
-
-        for (int i = 0; i < size; i++)
-        {
-            Destroy(tiles[i]);
-        }
-
-        tiles.Clear(); // removes all tiles
-    }
-
 
     public void BuildMap()
     {
-        var offset = new Vector3(20.0f, 0.0f, 20.0f);
 
-        // place the start tile
-        tiles.Add(Instantiate(startTile, Vector3.zero, Quaternion.identity, parent));
-
-        // choose a random goal position, cannot be equal the start and cannot be larger than the grid
-        var randomGoalRow = Random.Range(1, depth + 1);
-        var randomGoalCol = Random.Range(1, width + 1);
-
-        // generate more tiles if both width and depth are both greater than 2
-        for (int row = 1; row <= depth; row++)
+        //Generate first 3 tiles
+        for (int i = 0; i < 3; i++)
         {
-            for (int col = 1; col <= width; col++)
-            {
-                if (row == 1 && col == 1) {continue;}
+            var randomPrefabIndex = Random.Range(0, 4);
+            var randomRotation = Quaternion.Euler(0.0f, Random.Range(0, 4) * 90.0f, 0.0f);
+            //var randomTileIndex = Random.Range(0, 4);
+            //var newPosition = new Vector3();
 
-                var tilePosition = new Vector3(col * 20.0f, 0.0f, row * 20.0f) - offset;
+            //var randomRotation = Quaternion.Euler(0.0f, Random.Range(0, 4) * 90.0f, 0.0f);
 
-                if (row == randomGoalRow && col == randomGoalCol)
-                {
-                    // place the goal tile
-                    tiles.Add(Instantiate(goalTile, tilePosition, Quaternion.identity, parent));
-                }
-                else
-                {
-                    var randomPrefabIndex = Random.Range(0, 4);
-                    var randomRotation = Quaternion.Euler(0.0f, Random.Range(0, 4) * 90.0f, 0.0f);
-                    tiles.Add(Instantiate(tilePrefabs[randomPrefabIndex], tilePosition, randomRotation, parent));
-                }
-                
-            }
+            // tiles.Instantiate(tiles[randomTileIndex], newPosition, randomRotation);
 
+            tiles.Add(Instantiate(tilePrefabs[randomPrefabIndex], Vector3.zero, randomRotation));
         }
 
-       
-    }
+            tiles[0].transform.position = new Vector3(0.0f, 0.0f, 20.0f);
+            tiles[1].transform.position = new Vector3(20.0f, 0.0f, 20.0f);
+            tiles[2].transform.position = new Vector3(20.0f, 0.0f, 0.0f);
+        
+        for (int row = 1; row <= depth; row++)
+            {
+                for (int col = 1; col <= width; col++)
+                {
+                }
+            }
+        }
+
+
+    
 }
