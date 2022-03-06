a331f0715c5a480707836524ff63246822021708
Maria Albufera <malbufer@my.centennialcollege.ca>
2022-03-05 21:32:23 -0500

    Add my Week 7 Reset Function

--- WEEK7-COMP397/Assets/[Scripts]/MapGenerator.cs
+++ WEEK7-COMP397/Assets/[Scripts]/MapGenerator.cs
@@ -17,25 +17,47 @@ public class MapGenerator : MonoBehaviour
     [Header("Generated Tiles")]
     public List<GameObject> tiles;
 
+    private int startWidth;
+    private int startDepth;
 
     // Start is called before the first frame update
     void Start()
     {
+        startWidth = width;
+        startDepth = depth;
         BuildMap();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (width != startWidth || depth != startDepth)
+        {
+            ResetMap();
+            BuildMap();
 
+        }
     }
 
+    public void ResetMap()
+    {
+        startWidth = width;
+        startDepth = depth;
+        var size = tiles.Count;
+
+        for (int i = 0; i < size; i++)
+        {
+            Destroy(tiles[i]);
+        }
+
+        tiles.Clear(); // removes all tiles
+    }
     public void BuildMap()
     {
-
+        var offset = new Vector3( 20.0f, 0.0f, 20.0f);
         // generate more tiles if both width and depth are both greater than 2
-        
-            for (int row = 0; row <= depth; row++)
+
+        for (int row = 0; row <= depth; row++)
             {
 
                 for (int col = 0; col <= width; col++)
@@ -44,7 +66,7 @@ public class MapGenerator : MonoBehaviour
 
                     var randomPrefabIndex = Random.Range(0, 4);
                     var randomRotation = Quaternion.Euler(0.0f, Random.Range(0, 4) * 90.0f, 0.0f);
-                    var tilePositiion = new Vector3(col * 20.0f, 0.0f, row * 20.0f);
+                    var tilePositiion = new Vector3(col * 20.0f, 0.0f, row * 20.0f) - offset;
                     tiles.Add(Instantiate(tilePrefabs[randomPrefabIndex], tilePositiion, randomRotation, parent));
                 }
 
