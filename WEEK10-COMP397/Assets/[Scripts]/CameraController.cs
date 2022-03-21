a479327aeffe3a821d1cf6579bb944373704a720
Maria Albufera <malbufer@my.centennialcollege.ca>
2022-03-21 17:05:19 -0400

    Adding Week10 Lab 10 Procedural World

---
+++ WEEK10-COMP397/Assets/[Scripts]/CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 1000.0f;
+    public Transform playerBody;
+    public Joystick rightJoystick;
+
+    private float XRotation = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity + rightJoystick.Horizontal;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity + rightJoystick.Vertical;
+        float mouseX = rightJoystick.Horizontal;
+        float mouseY = rightJoystick.Vertical;
+
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
+
+        transform.localRotation = Quaternion.Euler(XRotation, 0.0f, 0.0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
