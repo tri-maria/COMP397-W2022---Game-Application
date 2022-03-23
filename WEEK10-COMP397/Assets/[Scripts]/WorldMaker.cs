5fe9777ac9ce2a64d4bd55eaa059b6285489d7bc
Maria Albufera <malbufer@my.centennialcollege.ca>
2022-03-23 00:05:58 -0400

    Adding Week10 Lab 10 Procedural World tiled

--- WEEK10-COMP397/Assets/[Scripts]/WorldMaker.cs
+++ WEEK10-COMP397/Assets/[Scripts]/WorldMaker.cs
@@ -5,35 +5,38 @@ using UnityEngine;
 public class WorldMaker : MonoBehaviour
 {
     [Header("World Properties")]
-    [Range(1, 5)]
+    [Range(1, 16)]
     public int height = 1;
-    [Range(1, 5)]
+    [Range(1, 64)]
     public int width = 1;
-    [Range(1, 5)]
+    [Range(1, 64)]
     public int depth = 1;
 
 
     [Header("Tile Properties")]
+    public Transform tileParent;
     public GameObject threeDTile;
-    public float size = 10;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        float rand = Random.Range(16.0f, 24.0f);
-
         float offsetX = Random.Range(-1024.0f, 1024.0f);
         float offsetZ = Random.Range(-1024.0f, 1024.0f);
         //generation
         for (int y = 0; y < height; y++)
         {
+            float rand = Random.Range(16.0f, 24.0f);
+
+            
             for (int z = 0; z < depth; z++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    if (y < Mathf.PerlinNoise((x + offsetX) / rand, (z + offsetZ) / rand) * depth * 0.5f)
+                    if (0.5f < Mathf.PerlinNoise((x + offsetX) / rand, (z + offsetZ) / rand)) // * depth * 0.5f)
                     {
-                        Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
+                        var tile=Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
+                         tile.transform.parent = tileParent;
                     }
 
                 }
