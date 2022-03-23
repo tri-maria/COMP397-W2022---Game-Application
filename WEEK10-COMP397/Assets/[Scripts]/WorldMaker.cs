4355ad5b6ec74f21e0d7084e2dccf3b520355db4
Maria Albufera <malbufer@my.centennialcollege.ca>
2022-03-23 14:58:09 -0400

    Adding Week10 Lab 10 Generate

--- WEEK10-COMP397/Assets/[Scripts]/WorldMaker.cs
+++ WEEK10-COMP397/Assets/[Scripts]/WorldMaker.cs
@@ -12,31 +12,73 @@ public class WorldMaker : MonoBehaviour
     [Range(1, 64)]
     public int depth = 1;
 
+    [Header("Scaling Values")]
+    public float min = 16.0f;
+    public float max = 24.0f;
 
     [Header("Tile Properties")]
     public Transform tileParent;
     public GameObject threeDTile;
-   
+
+    [Header("Grid")]
+    public List<GameObject> grid;
+
+    // starting values
+    private int startHeight;
+    private int startWidth;
+    private int startDepth;
+    private float startMin;
+    private float startMax;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        grid = new List<GameObject>(); // creates a new empty container
+
+        Generate();
+
+    }
+
+    private void Generate()
+    {
+        Initialize();
+        Reset();
+        Regenerate();
+    }
+
+    private void Initialize()
+    {
+        startHeight = height;
+        startWidth = width;
+        startDepth = depth;
+        startMin = min;
+        startMax = max;
+    }
+
+    private void Regenerate()
+    {
+        //generation
+        //perlin Noise texture that will sample
+        float rand = Random.Range(min, max);
+
         float offsetX = Random.Range(-1024.0f, 1024.0f);
         float offsetZ = Random.Range(-1024.0f, 1024.0f);
-        //generation
+
         for (int y = 0; y < height; y++)
         {
-            float rand = Random.Range(16.0f, 24.0f);
 
-            
+
+
             for (int z = 0; z < depth; z++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    if (0.5f < Mathf.PerlinNoise((x + offsetX) / rand, (z + offsetZ) / rand)) // * depth * 0.5f)
+                    if (y < Mathf.PerlinNoise((x + offsetX) / rand, (z + offsetZ) / rand) * depth * 0.5f)
                     {
-                        var tile=Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
-                         tile.transform.parent = tileParent;
+                        var tile = Instantiate(threeDTile, new Vector3(x, y, z), Quaternion.identity);
+                        tile.transform.parent = tileParent;
+                        grid.Add(tile);
                     }
 
                 }
@@ -44,9 +86,29 @@ public class WorldMaker : MonoBehaviour
             }
         }
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (height != startHeight || depth != startDepth || width != startWidth || min != startMin || max != startMax)
+        {
+            Generate();
+        }
+
+      /*  if (Input.GetKeyDown(KeyCode.R))
+        {
+            Generate();
+        }
+      */
+    }
+
+    private void Reset()
+    {
+        foreach (var tile in grid)
+        {
+            Destroy(tile);
+        }
+
+        grid.Clear();
     }
 }
